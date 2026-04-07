5c41b150d5d993e43ee7f0b0a5b3c54237cafcd8
vksuvam <vksuvam@gmail.com>
2026-04-08 00:14:32 +0530

    updated video_stats.py and added data

--- video_stats.py
+++ video_stats.py
@@ -2,6 +2,7 @@ import requests
 import json
 import os
 from dotenv import load_dotenv
+from datetime import date
 
 load_dotenv(dotenv_path = './.env')
 
@@ -94,10 +95,64 @@ def get_video_id(playlistId):
 
 
 
+def extract_video_data(video_ids):
+    extracted_data = []
+
+    def batch_list(video_id_lst, batch_size):
+        for video_id in range(0, len(video_id_lst), batch_size):
+            yield video_id_lst[video_id: video_id + batch_size]
+        
+    try:
+        for batch in batch_list(video_ids, MAXRESULTS):
+            video_id_str = ",".join(batch)
+
+            url = f'https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=statistics&part=snippet&id={video_id_str}&key={API_KEY}'
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['id']
+                snippet = item['snippet']
+                contentDetails = item['contentDetails']
+                statistics = item['statistics']
+            
+                video_data = {
+                    "video_id": video_id,
+                    "title": snippet['title'],
+                    "publishedAt": snippet['publishedAt'],
+                    "duration": contentDetails['duration'],
+                    "viewCount":statistics.get('viewCount', None),
+                    "likeCount":statistics.get('likeCount', None),
+                    "commentCount":statistics.get('commentCount', None)
+                }
+
+                extracted_data.append(video_data)
+
+        return extracted_data
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+
+def save_to_json(extracted_data):
+    file_path = f'./data/YT_data_{date.today()}.json'
+
+    with open(file_path, "w", encoding ="utf-8") as json_outfile:
+        json.dump(extracted_data, json_outfile, indent = 4, ensure_ascii = False)
+
 if __name__ == "__main__":
-    print("get playlist id will be executed")
+    print("Fetching Playlist ID from the YT channel")
     playlistId = get_playlist_id()
-    print(get_video_id(playlistId))
+    print("Fetching list of Video ID")
+    video_ids = get_video_id(playlistId)
+    print("Extracting video data")
+    video_data = extract_video_data(video_ids)
+    print("Saving extracted video data in json file")
+    save_to_json(video_data)
 
 # get_playlist_id()
 
